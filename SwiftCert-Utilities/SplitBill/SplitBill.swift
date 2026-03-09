0bc84a20b4ff5acbff659f1e837ad575ec52f87b
Karson Mellott <karsonmellott@gmail.com>
2026-03-09 09:45:09 -0600

    Added SplitBill file

---
+++ SwiftCert-Utilities/SplitBill/SplitBill.swift
@@ -0,0 +1,18 @@
+//
+//  SplitBill.swift
+//  SwiftCert-Utilities
+//
+//  Created by Karson Mellott on 3/9/26.
+//
+
+import SwiftUI
+
+struct SplitBill: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplitBill()
+}
